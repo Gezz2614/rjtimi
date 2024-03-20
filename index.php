bf60e347a79927ce741835580c3af5ff6ff40ea8
Gezz2614 <163402895+Gezz2614@users.noreply.github.com>
2024-03-20 14:44:43 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,12 +1,20 @@
-<!doctype html>
-<html lang=en>
-<head>
-<title>Template for the home page of an interactive database</title>
-<meta charset=utf-8>
-<link rel="stylesheet" type="text/css" href="includes.css">
-</head>
-<body>
-<div id='container'>
-<?php include('header.php'); ?><!--include the new header file-->
-<?php include('nav.php'); ?>
-<?php include('info-col.php'); ?>
+
+	<?php include('common/header.php') ?>
+	<div class="home-div"></div>
+		<div class="w-100 in-ad-ap">
+			<div class="row m-auto text-center">
+				<div class="col-md-4"><h3>INTRODUCING-CHMSC</h3></div>
+				<div class="col-md-4"><h3>ADMISSION POLICY</h3></div>
+				<div class="col-md-4"><h3>APPLY NOW</h3></div>
+			</div>
+		</div>
+		<div class="paragraph">
+			<p>
+				CHMSC provides a harmonious environment and learning opportunities  to its students regardless of their gender, socioeconomic background, religious beliefs and regional differences.
+			</p>
+		</div>
+	<?php include('common/cards.php') ?>
+	<?php include('common/footer.php') ?>
+</body>
+</html>
+
